e9edde2fa689dbcdecef0ba350ca2d670cd0feae
Irina Romjalis <iromjalis@gmail.com>
2021-02-18 12:47:17 +0200

    +

--- js/task_6.js
+++ js/task_6.js
@@ -1,67 +1,36 @@
 // Напиши скрипт, который бы при потере фокуса на инпуте, проверял его содержимое на правильное количество символов.
 
-// <input
-//   type="text"
-//   id="validation-input"
-//   data-length="6"
-//   placeholder="Введи 6 символов"
-// />
+{
+  /* <input
+  type="text"
+  id="validation-input"
+  data-length="6"
+  placeholder="Введи 6 символов"
+/> */
+}
 // Сколько символов должно быть в инпуте, указывается в его атрибуте data-length.
 // Если введено подходящее количество, то border инпута становится зеленым, если неправильное - красным.
 // Для добавления стилей, используй CSS-классы valid и invalid.
 
-// #validation-input {
-//   border: 3px solid #bdbdbd;
-// }
+let inputEl = document.querySelector("#validation-input");
+let idEl = inputEl.id;
 
-// #validation-input.valid {
-//   border-color: #4caf50;
-// }
+inputEl.addEventListener("input", onInputChange);
 
-// #validation-input.invalid {
-//   border-color: #f44336;
-// }
-// const inputEl = document.querySelector("#validation-input");
-// console.log(inputEl);
-// const inputLength = document.querySelector("#validation-input");
-// inputEl.addEventListener("blur", onInputBlur);
+let inputLength = inputEl.dataset.length;
+console.log(Number(inputLength)); //6
 
-// let outputEl = document.querySelector("#name-output");
+function onInputChange(event) {
+  // console.log("Инпут изменился");
+  inputEl = event.currentTarget.value;
 
-// function onInputChange(event) {
-//   inputEl = event.currentTarget.value;
-//   console.log(inputEl);
-// }
-
-// function onInputBlur() {
-//   console.log("Инпут потерял фокус - событие blur");
-// inputEl = currentTarget.value;
-
-//   console.log(inputEl);
-
-//   if (inputEl.length === 6) {
-//     inputEl.classList.add(".valid");
-//   } else inputEl.classList.add(".invalid");
-// }
-//!==============================================
-const clearLogBtn = document.querySelector('button[data-action="clear"]');
-const logList = document.querySelector(".log-list");
-
-window.addEventListener("keydown", logMessage);
-// window.addEventListener("keyup", logMessage);
-
-clearLogBtn.addEventListener("click", (e) => {
-  logList.innerHTML = "";
-});
-
-function logMessage({ type, key, code }) {
-  const message = document.createElement("section");
-  const title = document.createElement("h2");
-  title.textContent = `${type} event`;
-
-  const text = document.createElement("p");
-  text.textContent = `key value is "${key}" | code value is "${code}"`;
-
-  message.append(title, text);
-  logList.appendChild(message);
+  console.log("inputEl", inputEl); //то что ввожу
+  console.log(inputEl.length); //длина  введенного
+  console.log(inputEl.length === inputLength);
+  if (inputEl.length === Number(inputLength) && input.length !== null) {
+    console.log("hello");
+    inputEl.classList.add("#valid");
+    console.log(inputEl.id);
+  }
+  inputEl.classList.add("#invalid");
 }
