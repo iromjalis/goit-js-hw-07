cd0dec2a287d232d0f13204859cfdd208f4c1209
Irina Romjalis <iromjalis@gmail.com>
2021-03-01 19:44:03 +0200

    пробы сделатьь 8 дз
    созздает одинаковые квадраты при клике на кнопку

--- js/task-8.js
+++ js/task-8.js
@@ -22,14 +22,54 @@
 <div id="boxes"></div> */
 }
 
-const boxes = document.querySelector("boxes");
-// console.log(boxes);
-const renderBtn = document.querySelector("[data-action = 'render']");
-const destroyBtn = document.querySelector("[data-action = 'destroy']");
-// console.log(destroyBtn);
-destroyBtn.addEventListener("click", (e) => {
-  input.valuo = 0;
-});
+const boxes = document.createElement('div');
+boxes.classList.add('boxes');
+document.querySelector('#controls').appendChild(boxes);
 
-let random = Math.round(Math.random() * 255);
-// box.style.backgroundColor = `rgb(${random},${random},${random})`;
+const input = document.querySelector('#controls').children[0];
+console.log(input.value);
+
+const createBtn = document.querySelector('[data-action="render"]');
+console.log(createBtn);
+
+const removeBtn = document.querySelector('[data-action="destroy"]');
+console.log(removeBtn);
+let boxQuantity;
+
+input.addEventListener('change', onValueInput);
+
+function onValueInput(event) {
+  // console.log(event.currentTarget.value);
+  boxQuantity = event.currentTarget.value;
+  console.log(boxQuantity);
+  return boxQuantity;
+}
+
+createBtn.addEventListener('click', () => createBoxes(boxQuantity));
+removeBtn.addEventListener('click', () => removeBoxes(boxQuantity));
+
+function createBoxes(amount) {
+  const box = document.createElement('div');
+  let width = 30;
+  let height = 30;
+  box.style.width = `${width}px`;
+  box.style.height = `${height}px`;
+  let x = Math.floor(Math.random() * 256);
+  let y = Math.floor(Math.random() * 256);
+  let z = Math.floor(Math.random() * 256);
+  box.style.backgroundColor = `rgb(${x}, ${y}, ${z})`;
+  if (amount === 1) {
+    // boxes.append(box);
+  }
+  if (amount >= 1) {
+    for (let i = 0; i <= amount; i += 1) {
+      width += 10;
+      height += 10;
+      boxes.append(box);
+    }
+  }
+}
+
+function removeBoxes() {
+  boxes.remove();
+}
